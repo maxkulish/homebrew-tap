1df3ad8170f0420b31706bd859aba40ec66dea26
Max Kulish <kma.memo@gmail.com>
2026-05-26 18:53:27 +0200

    Bump rs-wisper to v0.23.9

--- Casks/rs-wisper.rb
+++ Casks/rs-wisper.rb
@@ -1,6 +1,6 @@
 cask "rs-wisper" do
-  version "0.23.6"
-  sha256 "85208ddb1642c3ce04d2e737e577c8f6a580a0944729ca271eb97b50817ad9ba"
+  version "0.23.9"
+  sha256 "b4e03017809f672d9156c0d9095fb3434ed37909a0a3b2377d9034748d467494"
 
   url "https://wisper.kulish.org/v#{version}/rs-wisper_#{version}_aarch64.dmg"
   name "rs-wisper"
