12ba37c1ebff0115c94ee2f7c0d9ac3150ba7135
Max Kulish <kma.memo@gmail.com>
2026-09-18 22:24:12 +0200

    Bump lok to v20260913.0.0

--- Formula/lok.rb
+++ Formula/lok.rb
@@ -1,21 +1,21 @@
 class Lok < Formula
   desc "Declarative multi-LLM orchestration across Claude, Codex, Gemini and Ollama"
   homepage "https://github.com/maxkulish/lok"
-  version "20260915.0.0"
+  version "20260913.0.0"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/maxkulish/lok/releases/download/v20260915.0.0/lok-v20260915.0.0-aarch64-apple-darwin.tar.gz"
-    sha256 "c5bcdb5fabd240f7c065df2b8830cae846bc88ea3cb4f64fd6d02d3aca2488d3"
+    url "https://github.com/maxkulish/lok/releases/download/v20260913.0.0/lok-v20260913.0.0-aarch64-apple-darwin.tar.gz"
+    sha256 "b59ab738e1918de11579e5f7158107c6451cdaefac2abdeabc1228b6f101d448"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/maxkulish/lok/releases/download/v20260915.0.0/lok-v20260915.0.0-x86_64-apple-darwin.tar.gz"
-    sha256 "07cab38381adc00cb9dafa0afdd11b10d95a0d14a8f94a6bbefb8ae5e90b7dc6"
+    url "https://github.com/maxkulish/lok/releases/download/v20260913.0.0/lok-v20260913.0.0-x86_64-apple-darwin.tar.gz"
+    sha256 "abcf7d4d03f008bac092522e3858b4c2269f868f937cacf1ec0691df7e685475"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/maxkulish/lok/releases/download/v20260915.0.0/lok-v20260915.0.0-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "9bf90b7dcc3331dbdae08ca3d163192b75ff7661fed5c8f20441275d0edabe0d"
+    url "https://github.com/maxkulish/lok/releases/download/v20260913.0.0/lok-v20260913.0.0-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "05c07268d23bf1c1cfaab3b4135c2036a9ae63cc5cdb2f5f9e61189b2bca023f"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/maxkulish/lok/releases/download/v20260915.0.0/lok-v20260915.0.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "d71b270e110be6ec879374d7060b01df1f43229cf00ab7fa51fe1f1b0565dbbe"
+    url "https://github.com/maxkulish/lok/releases/download/v20260913.0.0/lok-v20260913.0.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "37f11af166362cfddbb4efd1b79a922d026429be7a863bc94f4ceb573165a896"
   end
 
   def install
